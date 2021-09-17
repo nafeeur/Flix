5c87c07e91645b405f7ff5a3e2edf765d2b7de6a
Nafeeur Rahman <67729867+nafeeur@users.noreply.github.com>
2021-09-16 21:32:50 -0400

    Add files via upload

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Nafeeur on 9/16/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+
+
+}
